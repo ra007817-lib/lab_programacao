0a7fe7371df50e309d8a96dab01477cedb08fa0a
Leonardo de Oliveira Prado <007817@libertas.edu.br>
2026-06-25 21:10:47 -0300

    feat: nova atividade do dia 25 de jun de 2026

---
+++ atividades/Subprogramacao/ex004.py
@@ -0,0 +1,8 @@
+def exibir_cabecalho(texto):
+    print("********************")
+    print(texto)
+    print("********************")
+
+
+# Programa principal
+exibir_cabecalho("Sistema de Cadastro")
